ba0fa06866abac8c54c96cb5b93262b9dff8c4e6
imranahmed1990 <imranmarin1990@gmail.com>
2022-04-04 00:51:03 +0600

    my first commit for on this projects

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to="/home">HOME</Link>
+                <Link to="/reviewpage">REVIEWPAGE</Link>
+                <Link to="/deshboard">DESHBOARD</Link>
+                <Link to="/blogs">BLOGS</Link>
+                <Link to="/about">ABOUT</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
