36ddeac2c4531e27a6ad12684a1cb494fffeded5
imranahmed1990 <imranmarin1990@gmail.com>
2022-04-04 23:12:36 +0600

    about section commit done

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import img from '../../img/google.png';
+import './About.css';
 
 const About = () => {
     return (
+
         <div>
-            <h2>hellow about</h2>
+            <h2 className='text-content'>CONTACT:</h2>
+            <p className='p-text'>Please fill out the form on this section to contact with me. Or call between 9:00 a.m. and 8:00 p.m. ET, Monday through Friday</p>
+            <div className='about'>
+                <div className='input-container' >
+                    <p><input type="text" placeholder='Name' /></p>
+                    <p><input type="email" name="" id="" placeholder='Email' required /></p>
+                    <p className='message'><input type="text" placeholder='Message' /></p>
+                    <button className='submit-btn'>SUBMIT</button>
+                </div>
+                <div className='img-container'>
+                    <img src={img} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import img from '../../img/header-img.jpg';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h2>this is home components</h2>
+        <div className='home-container'>
+            <div className='container'>
+                <h1 className='heading'>WELCOME TO LAPTOP ANALYSIS WEBSITE</h1>
+                <p><small>Welcome to my laptop analysis website. This will be helping to laptop lover people.because we are discuss more about laptop information. So that we can help you about your usecase and more knowledge. please stay with us ....</small></p>
+                <button className='btn'>DETAILE INFORMATIN</button>
+            </div>
+            <div className='container'>
+                <img src={img} alt="" />
+            </div>
         </div>
     );
 };
