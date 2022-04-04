efe5626b3799b83c236f42c7a1325e09a02f02a9
imranahmed1990 <imranmarin1990@gmail.com>
2022-04-04 21:40:08 +0600

    404 page commit done

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import img from '../../img/not-found (2).png';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>Page not found</h2>
+        <div className='page-not-found'>
+            <img src={img} alt="" />
         </div>
     );
 };
